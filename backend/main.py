ddc23d36c4af5c9110c02fa426279854b4ba1b81
AKHIL SAURABH <akhilsaurabh65@gmail.com>
2026-06-12 10:57:41 +0530

    Add Response import to main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 from typing import Dict, List
